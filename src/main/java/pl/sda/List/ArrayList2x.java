2506fe8aab2daa2b249e45647cd32c09d23afd9a
Paweł Machoś <pawmach7@gmail.com>
2019-08-31 12:45:44 +0200

    Linked List and Array List w/ positive Tests

--- src/main/java/pl/sda/List/ArrayList2x.java
+++ src/main/java/pl/sda/List/ArrayList2x.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class ArrayList2x implements IList {
 
-    private final long[] holder;
+    private long[] holder;
     private int size = 0;
     private final int arraySizeMultipler = 2;
 
     public ArrayList2x() {
         holder = new long[10];
     }
+
 
     // to do
     @Override
@@ -26,24 +27,24 @@ public class ArrayList2x implements IList {
 
     @Override
     public long get(int index) {
+        checkBounds(index);
         return holder[index];
     }
 
     //to do
     @Override
     public void set(int index, long value) {
-
-        if (index < holder.length) {
+        checkBounds(index);
             holder[index] = value;
-        }
+
     }
 
     //to do
     @Override
     public void remove(int index) {
-
-        for (int i = index; i < holder.length - 1; i++) {
-            holder[i] = holder[i - 1];
+        checkBounds(index);
+        for (int i = index; i < size - 1; i++) {
+            holder[i] = holder[i + 1];
         }
         size--;
     }
@@ -58,22 +59,50 @@ public class ArrayList2x implements IList {
         return -1;
     }
 
-    //to do - increment size of table
-    @Override            //   0             2           size= 1
+    @Override
     public void add(int index, long value) {
+        checkInsertBounds(index);
+        if (holder.length == size)
+            incrementHolderLength();
+
+        for (int i = size; i > index; i--) {
+            holder[i] = holder[i - 1];
+        }
+
+        holder[index] = value;
+        size++;
+    }
+
+    //to do - increment size of table
+    /*@Override            //   0             2           size= 1
+    public void add(int index, long value) {
+
         for (int i = size; i > index; i--) {
             holder[i] = holder[i - 1];
         }
         holder[index] = value;
         size++;
-    }
+    }*/
 
     //to do - increment size of physical table
+    private void incrementHolderLength() {
+        int newHolderSize = holder.length * arraySizeMultipler;
+        long[] newHolder = new long[newHolderSize];
+        for (int i = 0; i < size; i++) {
+            newHolder[i] = holder[i];
+            holder = newHolder;
+        }
+    }
+
     @Override
     public void add(long value) {
+        if (holder.length == size) {
+            incrementHolderLength();
+        }
         holder[size] = value;
         size++;
     }
+
 
     @Override
     //TODO - This should return copy of the filled part of the array
@@ -81,4 +110,16 @@ public class ArrayList2x implements IList {
         return Arrays.copyOfRange(holder, 0, size);
     }
 
+
+    private void checkBounds(int index) {
+        if (index < 0 || index >= holder.length) {
+            throw new IndexOutOfBoundsException(index);
+        }
+    }
+
+    private void checkInsertBounds(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+    }
 }
