28ab038e6bc0548cb123075db16a31e30d1dc671
Paweł Machoś <pawmach7@gmail.com>
2019-09-01 10:44:03 +0200

    selection sort for IList

--- src/main/java/pl/sda/List/MainList.java
+++ src/main/java/pl/sda/List/MainList.java
@@ -3,14 +3,21 @@ package pl.sda.List;
 public class MainList {
 
     public static void main(String[] args) {
+        SelectionSort ss = new SelectionSort();
         IList list = new LinkedList();
+        list.add(5);
         list.add(1);
         list.add(2);
-        list.add(3);
+        list = ss.sort(list);
+
 
         Object[] tab = list.getHolderView();
 
-        for (Object val:tab)
+        for (Object val : tab){
             System.out.print(val + " ");
+    }
+        System.out.println();
+
+        
     }
 }
--- src/main/java/pl/sda/List/SelectionSort.java
+++ src/main/java/pl/sda/List/SelectionSort.java
@@ -2,6 +2,21 @@ package pl.sda.List;
 
 public class SelectionSort {
 
+    public IList sort(IList<Integer> listToSort){
+        int firstUnsortedIndex = 0;
+
+        do {
+            int minIndex = findMinValueIndex(listToSort, firstUnsortedIndex);
+            int tmp = listToSort.get(minIndex);
+            listToSort.set(minIndex, listToSort.get(firstUnsortedIndex));
+            listToSort.set(firstUnsortedIndex,tmp);
+        }
+        while(++firstUnsortedIndex != listToSort.size());
+        return listToSort;
+    }
+
+
+
 
     public int[] sort(int[] tableToSort) {
         int firstUnsortedIndex = 0;
@@ -15,6 +30,18 @@ public class SelectionSort {
         while(++firstUnsortedIndex != tableToSort.length);
         return tableToSort;
 
+    }
+
+    private int findMinValueIndex(IList<Integer> list, int startIndex) {
+        int minValue = list.get(startIndex);
+        int minValueIndex = startIndex;
+        for (int i = startIndex + 1; i < list.size(); i++){
+            if (list.get(i)<minValue){
+                minValue = list.get(i);
+                minValueIndex = i;
+            }
+        }
+        return minValueIndex;
     }
 
 // 1 2 5 7 4(3) 8 11 0
